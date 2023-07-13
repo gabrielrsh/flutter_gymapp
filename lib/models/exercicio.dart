2ced95fed481e94b92f9cdf4111fed760a595587
gabrielrsh <gabriel.shiota@gmail.com>
2023-07-13 10:53:34 -0300

    att

---
+++ lib/models/exercicio.dart
@@ -0,0 +1,31 @@
+class Exercicio {
+  String id;
+  String nome;
+  String treino;
+  String comoFazer;
+  String? urlImagem;
+
+  Exercicio({
+    required this.id,
+    required this.nome,
+    required this.treino,
+    required this.comoFazer,
+  });
+
+  Exercicio.fromMap(Map<String, dynamic> map)
+      : id = map['id'],
+        nome = map['nome'],
+        treino = map['treino'],
+        comoFazer = map['comoFazer'],
+        urlImagem = map['urlImagem'];
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'nome': nome,
+      'treino': treino,
+      'comoFazer': comoFazer,
+      'urlImagem': urlImagem
+    };
+  }
+}
--- lib/pages/exercicio_page.dart
+++ lib/pages/exercicio_page.dart
@@ -6,40 +6,39 @@ class ExercicioPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Exercicio 1 - Treino A'),
+        appBar: AppBar(
+          title: const Text('Exercicio 1 - Treino A'),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
           child: const Icon(Icons.add),
         ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-          ElevatedButton(
-            onPressed: (){},
-            child: const Text("Enviar Foto")
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ElevatedButton(
+                  onPressed: () {}, child: const Text("Enviar Foto")),
+              const Text(
+                "Como fazer?",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+              const Text("Explicação de como fazer o exercicio"),
+              const Divider(),
+              const Text(
+                "Como estou me sentindo?",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+              const Text("Explicação de como estou me sentindo"),
+            ],
           ),
-          const Text("Como fazer?", 
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 18,
-              ),
-            ),
-            const Text("Explicação de como fazer o exercicio"),
-            const Divider(),
-            const Text("Como estou me sentindo?", 
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 18,
-              ),
-            ),
-            const Text("Explicação de como estou me sentindo"),
-        ],
-        ),
-      )
-    );
+        ));
   }
 }
